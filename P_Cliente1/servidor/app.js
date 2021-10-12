5dc5f488d159fab5889ec15f0e7277a49df713bd
derJuanfer <juan.fernando.franco@correounivalle.edu.co>
2021-10-12 14:34:59 -0500

    Heroku_2

--- P_Cliente1/servidor/app.js
+++ P_Cliente1/servidor/app.js
@@ -21,6 +21,13 @@ mongoose.connect(uri, options).then(
 //Middleware
 app.use(morgan('tiny'));
 app.use(cors());
+app.use((req, res, next)=>{
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
+ res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+ res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+ next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
